866bff17d6cfaa4c9bd814374fbf1c175801cd9f
Your Name <you@example.com>
2017-11-15 11:03:16 +0100

    8 commit

---
+++ app/src/main/java/com/example/kamde/snowtamdecoder/SnowtamActivity.java
@@ -0,0 +1,13 @@
+package com.example.kamde.snowtamdecoder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SnowtamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snowtam);
+    }
+}
